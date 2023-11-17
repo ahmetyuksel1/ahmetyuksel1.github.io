92454f2dbf75cce4cdcdd9ff3c400dc54c5daddd
Ahmet Yüksel <92723248+ahmetyuksel1@users.noreply.github.com>
2023-11-17 14:08:44 +0300

    Added forms folder.

---
+++ forms/contact.php
@@ -0,0 +1,30 @@
+<?php
+  $receiving_email_address = 'ahmetyuksel@ahmetyuksel.com';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  $contact->smtp = array(
+    'host' => 'ahmetyuksel.com',
+    'username' => 'ahm3a6ukselcom_admin',
+    'password' => '1248729214841436514363636',
+    'port' => '587'
+  );
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
+?>
